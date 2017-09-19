f5ece0cb673175f3ff7599868e9b8a74df140fc7
Pete Russell <pinkbatz@gmail.com>
2017-09-19 11:15:21 -0500

    Adds ViewEncapsulation (CSS scope) example & explanation.

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  
+  // 'encapsulation' determines whether CSS styles are applied only to this
+  // component, or applied globally.
+  // Emulated is the default value, so it's not actually required to declare
+  // explicitly. Other values are None (use normal browser renderer rules),
+  // and Native, which uses the Shadow DOM. This works similarly to 'emulated',
+  // but not all browsers support Shadow DOM.
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class ServerElementComponent implements OnInit {
   @Input() element: { type: string, name: string, content: string };
